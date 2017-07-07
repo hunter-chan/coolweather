f01fda9b5a9c8307347aad4b37f08234bc2149f0
hunter-haha <891810979@qq.com>
2017-07-07 10:07:06 +0800

    完成一个城市的天气查询以及显示

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Admin on 2017/7/6.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+
+}
